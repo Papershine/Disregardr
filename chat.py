720773fbe4d9b54a175cc85852a79582175e9de1
Papershine <hilarylau12342@gmail.com>
2022-12-29 15:28:57 +0800

    chat system improvement

--- chat.py
+++ chat.py
@@ -3,6 +3,7 @@
 import chatexchange.client
 import chatexchange.events
 import getpass
+import chatcommands
 
 from chatexchange.rooms import Room
 
@@ -18,7 +19,7 @@ class ChatClient:
         host_id = 'stackoverflow.com'
         room_id = '250724'  # hardcoded test bot room
 
-        print("Beginning chat login")
+        print("[ChatScanner] Beginning chat login")
         email = input("Email: ")
         password = getpass.getpass("Password: ")
         self.client = chatexchange.client.Client(host_id)
@@ -27,7 +28,7 @@ class ChatClient:
         self.room = self.client.get_room(room_id)
         self.room.join()
         self.room.watch(on_message)
-        print("Joined room " + room_id + " on " + host_id + " and listening!")
+        print("[ChatScanner] Joined room %s on %s and listening!" % (room_id, host_id))
         self.room.send_message("[ Disregardr ] started!")
 
     def send_message(self, message):
@@ -37,10 +38,15 @@ class ChatClient:
 def on_message(message, client):
     if not isinstance(message, chatexchange.events.MessagePosted):
         # Ignore non-message_posted events.
-        print("event: %r", message)
+        print("[ChatScanner] event: %r", message)
         return
 
-    print("")
-    print(">> (%s) %s" % (message.user.name, message.content))
+    print("\n[ChatScanner] >> (%s) %s" % (message.user.name, message.content))
     if message.content.startswith('@Pape'):
-        print("Tagged!")
+        print("[ChatScanner] Command Received")
+        command = message.content.split(' ')[1:]
+        match command:
+            case ['alive']:
+                chatcommands.alive(message)
+            case _:
+                print("[ChatScanner] Unknown Command")
---
+++ chatcommands.py
@@ -0,0 +1,2 @@
+def alive(message):
+    message.message.reply("yes")
